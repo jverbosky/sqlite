3e40da203444917599989cfd5c8cab7e69ecc31d
jverbosk <jverbosk@gmail.com>
2017-03-30 14:22:42 -0400

    updated to insert and query multiple users and images to/from database

--- create_update_query_all/2_add_data_and_images.rb
+++ create_update_query_all/2_add_data_and_images.rb
@@ -1,48 +1,59 @@
+# note - images span user_1.png - user_8.png
+# so can run this twice to build DB w/duplicate user records if desired
+
 require 'sqlite3'
 
-# Collect file and data for database insertion
 begin
 
-  file_open = File.open './public/images/user_1.png' , 'rb'
-  image = file_open.read
-  blob = SQLite3::Blob.new image
+  # user data sets
+  user_1 = ["John", 41, 7, 11, 3, "Research is what I'm doing when I don't know what I'm doing."]
+  user_2 = ["Jane", 51, 1, 2, 3, "Life is 10% what happens to you and 90% how you react to it."]
+  user_3 = ["Jim", 61, 10, 20, 30, "In order to succeed, we must first believe that we can."]
+  user_4 = ["Jill", 71, 11, 22, 33, "It does not matter how slowly you go as long as you do not stop"]
 
-  data = ["John", 41, 7, 11, 3, "Research is what I'm doing when I don't know what I'm doing."]
+  # aggregate user data into multi-dimensional array for iteration
+  users = []
+  users.push(user_1, user_2, user_3, user_4)
 
-  v_name = data[0]
-  v_age = data[1]
-  v_num_1 = data[2]
-  v_num_2 = data[3]
-  v_num_3 = data[4]
-  v_quote = data[5]
-
-rescue SystemCallError => e
-
-  puts e
-
-ensure
-
-  file_open.close if file_open
-
-end
-
-# Open database file and insert new row with data and file
-begin
-
-  # reference
-  # db = SQLite3::Database.open 'details.db'
-  # blob = SQLite3::Blob.new img
-  # db.execute "insert into images values(1, ?)", blob
-
+  # open database for updating
   db = SQLite3::Database.open 'personal_details.db'
 
-  id_count = db.execute('select count(*) from details')
-  v_id = id_count[0][0] + 1
+  # determine current max index (id) in details table
+  db.results_as_hash
+  max_id = db.execute('select max("id") from details')[0][0]
 
-  db.execute('insert into details (id, name, age, num_1, num_2, num_3, quote)
-              values(?, ?, ?, ?, ?, ?, ?)', [v_id, v_name, v_age, v_num_1, v_num_2, v_num_3, v_quote])
-  db.execute('insert into images (id, details_id, image)
-              values(?, ?, ?)', [v_id, v_id, blob])
+  # set index variable based on current max index value
+  max_id == nil ? v_id = 1 : v_id = max_id + 1
+
+  # iterate through multi-dimensional users array for data
+  users.each do |user|
+
+    # initialize variables for SQL insert statements
+    v_name = user[0]
+    v_age = user[1]
+    v_num_1 = user[2]
+    v_num_2 = user[3]
+    v_num_3 = user[4]
+    v_quote = user[5]
+
+    # prepare image for database insertion
+    file_open = File.open "./public/images/user_#{v_id}.png" , 'rb'
+    image = file_open.read
+    file_open.close if file_open
+    blob = SQLite3::Blob.new image
+
+    # insert user data into details table
+    db.execute('insert into details (id, name, age, num_1, num_2, num_3, quote)
+                values(?, ?, ?, ?, ?, ?, ?)', [v_id, v_name, v_age, v_num_1, v_num_2, v_num_3, v_quote])
+
+    # insert user image into images table
+    db.execute('insert into images (id, details_id, image)
+                values(?, ?, ?)', [v_id, v_id, blob])
+
+    # increment index value for next iteration
+    v_id += 1
+
+  end
 
 rescue SQLite3::Exception => e
 
--- create_update_query_all/3_query_data_and_images.rb
+++ create_update_query_all/3_query_data_and_images.rb
@@ -5,19 +5,21 @@ begin
     db = SQLite3::Database.open 'personal_details.db'
     db.results_as_hash = true
 
-    # Example of getting column names
+    # example of getting column names
     d_columns = db.execute2('select * from details join images on details.id = images.details_id')[0]
     p d_columns  # ["id", "name", "age", "num_1", "num_2", "num_3", "quote", "id", "details_id", "image"]
 
-    # Example of getting all data from both tables in personal_details.db
+    # example of getting all data from both tables in personal_details.db
     # p db.execute('select * from details join images on details.id = images.details_id')
 
-    # Prepare data for iteration
+    # prepare data for iteration
     d_query = db.prepare('select * from details join images on details.id = images.details_id')
     d_rows = d_query.execute
 
-    # Output data to console and file to current directory for each row
+    # iterate through each row for user data and image
     d_rows.each do |row|
+
+      # output user data to console
       puts "Details ID: #{row['id']}"
       puts "Images ID: #{row['details_id']}"
       puts "Name: #{row['name']}"
@@ -26,11 +28,15 @@ begin
       puts "Favorite number 2: #{row['num_2']}"
       puts "Favorite number 3: #{row['num_3']}"
       puts "Quote: #{row['quote']}"
+
+      # output user image to current directory, named based on user name and ID
       image = row['image']
-      f = File.new "#{row['id']}_#{row['name']}_output.png", "wb"
+      f = File.new "#{row['name']}_#{row['id']}_output.png", "wb"
       f.write image
+      f.close if f
     end
 
+    # --- Example data ---
     # Details ID: 1
     # Images ID: 1
     # Name: John
@@ -39,6 +45,7 @@ begin
     # Favorite number 2: 11
     # Favorite number 3: 3
     # Quote: Research is what I'm doing when I don't know what I'm doing.
+    # --------------------
 
 rescue SQLite3::Exception, SystemCallError => e
 
@@ -46,6 +53,7 @@ rescue SQLite3::Exception, SystemCallError => e
     puts e
 
 ensure
-    f.close if f
+
     db.close if db
+
 end
